e8113b1120b26e92ed4e7ab9b2c59e185528a4a7
Christopher Houghton <ChrisHoughton@ChristopherMBP.fios-router.home>
2021-08-15 16:51:06 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  const task = document.getElementById('new-task-description');
+  const form = document.getElementById('create-task-form');
+  const list = document.getElementById('tasks')
+
+  function logSubmit(event) { 
+    event.preventDefault();
+    let li = document.createElement("li");
+    tasks.appendChild(li);
+    li.innerHTML = task.value; 
+    form.reset();
+  }
+
+  form.addEventListener('submit', logSubmit);
 });
